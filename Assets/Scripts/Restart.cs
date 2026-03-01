589265de0647db5cd41bac857e53056e8172d5d2
chameetutN <chameetut.inn@gmail.com>
2026-03-01 16:49:30 -0500

    Added restart to Dog scene to winscreen

--- Assets/Scripts/Restart.cs
+++ Assets/Scripts/Restart.cs
@@ -4,14 +4,9 @@ using UnityEngine.SceneManagement;
 public class Restart : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public void RestartGame()
     {
         SceneManager.LoadScene("Dog");
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
