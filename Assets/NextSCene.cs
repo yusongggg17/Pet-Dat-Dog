fa6a23220d1ff6cbf5c215433d9ac0175da7f673
jake0822 <148503198+jake0822@users.noreply.github.com>
2026-03-01 16:31:23 -0500

    Adds completed start sequence

---
+++ Assets/NextSCene.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor.Animations;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NextSCene : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        StartCoroutine(next());
+    }
+    IEnumerator next()
+    {
+        yield return new WaitForSeconds(12.5f);
+        print("next scene");
+        SceneManager.LoadScene(2);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
